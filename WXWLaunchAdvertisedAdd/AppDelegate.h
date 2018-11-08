c1fa8dd8cb0ab08d807cfac93ed512d53c977e1d
591807572@qq.com <591807572@qq.com>
2018-11-08 14:43:14 +0800

    Initial Commit

---
+++ WXWLaunchAdvertisedAdd/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WXWLaunchAdvertisedAdd
+//
+//  Created by administrator on 2018/11/8.
+//  Copyright © 2018年 xuewen.wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WXWLaunchAdvertisedAdd/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WXWLaunchAdvertisedAdd
+//
+//  Created by administrator on 2018/11/8.
+//  Copyright © 2018年 xuewen.wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
